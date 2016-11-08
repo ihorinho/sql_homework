39b138965fb4fdb404fff673f0af9a0c000e43fb
ihorinho <ihorinho@gmail.com>
2016-11-08 20:24:00 +0200

    Change port for sql

--- index.php
+++ index.php
@@ -3,13 +3,21 @@
 // admin@adminka.com, 'komarik333';
 // manager@adminka.com 'qweqwe111';
 //echo md5(md5('salt') . 'qweqwe111');
+
+//My settings
+// $link = mysqli_connect(
+//     "127.0.0.1:3307",
+//     "root",
+//     "",
+//     "mvc_group_609"
+// );
+
 $link = mysqli_connect(
-    "127.0.0.1:3307",
+    "127.0.0.1",
     "root",
     "",
     "mvc_group_609"
 );
-
 session_start();
 require 'inc/functions.php';
 // $_GET['page']. If not set, then = 'home'
